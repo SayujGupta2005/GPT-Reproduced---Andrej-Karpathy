cbaedad736e73386c724b2fed48e50d074d82078
Sayuj Gupta <144648538+SayujGupta2005@users.noreply.github.com>
2025-02-17 22:42:50 +0530

    Update transformer.py

--- transformer.py
+++ transformer.py
@@ -198,7 +198,24 @@ class GPTLanguageModel(nn.Module):
             logits=logits.view(B*T,C)
             targets=targets.view(B*T)
             loss=F.cross_entropy(logits,targets)
-        return logits,loss     
+        return logits,loss
+    def generate(self, idx, max_new_tokens):
+        idx = idx.to(self.token_embedding_table.weight.device)
+        # idx is (B, T) array of indices in the current context
+        for _ in range(max_new_tokens):
+            # crop idx to the last block_size tokens
+            idx_cond = idx[:, -block_size:]
+            # get the predictions
+            logits, loss = self(idx_cond)
+            # focus only on the last time step
+            logits = logits[:, -1, :] # becomes (B, C)
+            # apply softmax to get probabilities
+            probs = F.softmax(logits, dim=-1) # (B, C)
+            # sample from the distribution
+            idx_next = torch.multinomial(probs, num_samples=1) # (B, 1)
+            # append sampled index to the running sequence
+            idx = torch.cat((idx, idx_next), dim=1) # (B, T+1)
+        return idx
 
 # For training, comment everything after line 229
 # For eval, comment everything from line 206 to 229 and uncomment after that
